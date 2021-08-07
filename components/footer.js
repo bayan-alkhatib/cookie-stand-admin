cdffa21ccdb0dd7986d1156827e590647ecc687f
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-08-07 16:57:55 +0300

    complete lab 39

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer(props){
     return(
-      <footer className="flex justify-between bg-green-500 p-4 items-center mt-4">
-      <h2> {props.report.length} Locations World Wide </h2>
+      <footer className="items-center p-4 mt-4 text-center bg-green-500">
+      <h2 className='text-center'> {props.report.length} Locations World Wide </h2>
     </footer>
     ) 
   }
